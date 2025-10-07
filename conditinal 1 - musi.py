46e8d1f072fc30b65cdcc01de9e9f340990e21ae
musinicholas-debug <musinicholas@gmail.com>
2025-10-07 09:26:53 -0400

    Create conditinal 1 - musi.py

---
+++ conditinal 1 - musi.py
@@ -0,0 +1,7 @@
+i=input("Enter electric range")
+if i==0:
+        print("range has not been tested")
+elif i>=100:
+        print("long electric range")
+else:
+        print("short electric range")
