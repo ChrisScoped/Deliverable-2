35085dd5a9361d66c68e651f54a9853669690fab
ChrisScoped <christhebreacher@gmail.com>
2025-10-15 17:33:57 -0400

    Updated Scatterplot Code
    Scatterplot code for graph

--- Mileage Code - Gibara.py
+++ Mileage Code - Gibara.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
 
 # I'm for now using only 20 samples to avoid messy plots
-maker_col = df["Make"].head(20)
+maker_col = df["Make"].head(25)
 
 plt.hist(maker_col)
 
@@ -19,22 +18,10 @@ for make in maker_col:
 
 print(df["Base MSRP"])
 
-plt.figure(figsize=(18,6))
-sns.relplot(data=df, x=maker_col, y=df["Model Year"].head(20), hue="Electric Vehicle Type")
 
+x_data=df["Make"].head(25)
+y_data=df["Model Year"].head(25)
 
+plt.figure(figsize=(10,10))
+plt.scatter(x_data, y_data)
 
-
-
-
-
-#n=int(input("Enter Electric Range:"))
-#if n == 0:
-#  print("Range has not been tested")
-#elif n<= 100:
-#        print("Short Range")
-#elif n > 100 and n <= 300 :
-#        print("Average Range")
-#else:
-#        print("Long Range")        
-    
