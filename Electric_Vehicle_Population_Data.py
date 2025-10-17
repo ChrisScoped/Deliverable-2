a723ea23acc8c08d1379abbf5a40b144f5109272
musinicholas-debug <musinicholas@gmail.com>
2025-10-16 20:43:23 -0400

    part 3-musi

--- Electric_Vehicle_Population_Data.py
+++ Electric_Vehicle_Population_Data.py
@@ -3,3 +3,13 @@ import matplotlib.pyplot as plt
 
 # Load the filtered CSV dataset
 df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
+
+#part 3 for short or long range
+i=input("Enter electric range")
+if i==0:
+        print("range has not been tested")
+elif i>=100:
+        print("long electric range")
+else:
+        print("short electric range")
+
