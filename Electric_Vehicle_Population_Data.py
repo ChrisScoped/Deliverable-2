b9855d2850fbd491ac45ecfb76bce342a5173bf3
musinicholas-debug <musinicholas@gmail.com>
2025-10-16 21:13:05 -0400

    Histogram

--- Electric_Vehicle_Population_Data.py
+++ Electric_Vehicle_Population_Data.py
@@ -112,3 +112,20 @@ plt.pie(type_counts.values, labels=type_counts.index, autopct="%1.1f%%", startan
 plt.title("Distribution of Electric Vehicle Types")
 plt.show()
 
+#PART 4 histogram 
+# Convert relevant columns to numeric types (force invalid strings to NaN)
+df["Model Year"] = pd.to_numeric(df["Model Year"], errors="coerce")
+df["Electric Range"] = pd.to_numeric(df["Electric Range"], errors="coerce")
+df["Base MSRP"] = pd.to_numeric(df["Base MSRP"], errors="coerce")
+
+#Histrogram: Distribution of Electric Range
+# Purpose: compare electric range to number of vehicles
+ranges = df["Electric Range"].dropna()
+plt.figure(figsize=(8, 5))
+plt.hist(ranges, bins=30, color="skyblue", edgecolor="black")
+plt.title("Distribution of Electric Range")
+plt.xlabel("Electric Range (miles)")
+plt.ylabel("Count of Vehicles")
+plt.grid(axis="y", alpha=0.75)
+plt.show()
+
